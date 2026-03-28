3e712a6336f807cf78f854d71b560dfabdd152d1
BhawaniPrabhasha <prabhashadilrangi1111@gmail.com>
2026-03-28 10:31:03 +0530

    update socialsidebar

--- src/components/SocialSidebar.jsx
+++ src/components/SocialSidebar.jsx
@@ -1,53 +1,40 @@
-
 import React from "react";
 import {
   AiOutlineGithub,
   AiOutlineLinkedin,
   AiOutlineMail,
-  AiOutlineWhatsApp,
-  AiOutlineMessage,
 } from "react-icons/ai";
 
 export default function SocialSidebar() {
   return (
     <div className="fixed left-4 top-1/2 -translate-y-1/2 z-50 hidden md:flex flex-col gap-4">
       
+      {/* GitHub */}
       <a
-        href="https://github.com/yourusername"
+        href="https://github.com/BhawaniPrabhasha"
         target="_blank"
+        rel="noopener noreferrer"
         className="social-icon"
       >
         <AiOutlineGithub />
       </a>
 
+      {/* LinkedIn */}
       <a
-        href="https://www.linkedin.com/in/yourprofile"
+        href="https://www.linkedin.com/in/bhawani-prabhasha/"
         target="_blank"
+        rel="noopener noreferrer"
         className="social-icon"
       >
         <AiOutlineLinkedin />
       </a>
 
+      {/* Email */}
       <a
-        href="mailto:yourmail@gmail.com"
+        href="mailto:bhawaniprabhasha001@gmail.com"
         className="social-icon"
       >
         <AiOutlineMail />
-      </a>
-
-      <a
-        href="https://wa.me/947XXXXXXXX"
-        target="_blank"
-        className="social-icon"
-      >
-        <AiOutlineWhatsApp />
-      </a>
-
-      <a
-        href="#contact"
-        className="social-icon"
-      >
-        <AiOutlineMessage />
       </a>
 
     </div>
